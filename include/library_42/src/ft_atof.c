8fdef2d0b3eab93741934d3f420c12263c6becba
Sarah <115113929+smclacke@users.noreply.github.com>
2023-03-17 14:37:45 +0100

    Add files via upload

---
+++ include/library_42/src/ft_atof.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_atof.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/04 16:01:22 by smclacke      #+#    #+#                 */
+/*   Updated: 2023/03/11 16:15:11 by smclacke      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	check_sign(const char *str)
+{
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+	}
+	return (sign);
+}
+
+double	ft_atof(const char *str)
+{
+	int		i;
+	int		sign;
+	double	result;
+	double	decimal;
+
+	if (!str)
+		return (0);
+	i = 0;
+	result = 0.0;
+	decimal = 1.0;
+	if (str[i])
+	{
+		sign = check_sign(str);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + (str[i++] - '0');
+	if (str[i] == '.')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i++] - '0');
+		decimal *= 10;
+	}
+	return (sign * result / decimal);
+}
