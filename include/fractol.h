7e6e990fdd34172e110b89b65d16003248beaaa6
Sarah <115113929+smclacke@users.noreply.github.com>
2023-03-18 22:26:49 +0100

    Add files via upload

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/11 22:28:21 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/03/18 19:34:15 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/03/18 22:25:30 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,13 @@ typedef struct s_fractol {
 	enum e_colours		colour[3];
 	int					iter;
 	int					max_iter;
-	float				x[2];
-	float				y[2];
-	float				julia[2];
+	double				x[2];
+	double				y[2];
+	double				julia[2];
 	int					mouse[2];
-	float				xscale;
-	float				yscale;
-	float				scale;
+	double				xscale;
+	double				yscale;
+	double				scale;
 	double				zoom;
 }	t_fractol;
 
@@ -58,8 +58,8 @@ typedef struct s_fractol {
 
 void		colour_hook(t_fractol *data);
 void		ft_move(t_fractol *data, char direction);
-void		ft_zoom_in(t_fractol *data);
-void		ft_zoom_out(t_fractol *data);
+// void		ft_zoom_in(t_fractol *data);
+// void		ft_zoom_out(t_fractol *data);
 void		ft_key_hook(mlx_key_data_t keydata, t_fractol *data);
 void		ft_scroll_hook(double xdelta, double ydelta, t_fractol *data);
 
@@ -79,17 +79,17 @@ uint32_t	get_rgba(t_fractol *data, float iter);
 
 //----------Julia----------//
 
-float		init_julia(t_fractol *data, float iter, float x, float y);
+double		init_julia(t_fractol *data, double iter, double x, double y);
 void		julia(t_fractol *data);
 
 //----------Mandelbrot----------//
 
-float		init_mandelbrot(float iter, float x1, float y1);
+double		init_mandelbrot(double iter, double x1, double y1);
 void		mandelbrot(t_fractol *data);
 
 //----------Burningship----------//
 
-float		init_burning(float iter, float x1, float y1);
+double		init_burning(double iter, double x1, double y1);
 void		burningship(t_fractol *data);
 
 //----------Colour----------//
